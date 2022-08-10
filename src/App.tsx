1b886d290c9b91682b1dde123054d645e0cc35e6
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-08-09 22:48:36 -0500

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <a href="https://reactjs.org/" target="_blank">
+          React
+        </a>{" "}
+        +{" "}
+        <a href="https://www.typescriptlang.org/" target="_blank">
+          TS
+        </a>{" "}
+        +{" "}
+        <a href="https://vitejs.dev/" target="_blank">
+          Vite
+        </a>{" "}
+        +{" "}
+        <a href="https://chakra-ui.com/" target="_blank">
+          Chakra
+        </a>
+      </h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+      <p>
+        <i>
+          {"{"}Created by{" "}
+          <a href="https://www.abrahamcalsin.com" target="_blank">
+            Abraham Calsin
+          </a>
+          {"}"}
+        </i>
+      </p>
+    </div>
+  );
+}
+
+export default App;
